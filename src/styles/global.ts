9f362053fd94346a97e4079a21798d89b97d79e9
felipsbreno <brendo.filipe2050@gmail.com>
2022-11-15 22:11:29 -0300

    feat: add font poppins

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -30,7 +30,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
 
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
